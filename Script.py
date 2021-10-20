8444cb55cfc7a89e08abca0fc441d420babf1e03
Estevam <92314399+Estevam2302@users.noreply.github.com>
2021-10-20 14:28:13 -0300

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,5 @@
+print('Bem vindo ao conversor de temperatura Celsius para Fahrenheit!')
+celsius = int(input('Insira a temperatura em ºC: '))
+fahrenheit = int((9 * celsius) / 5) + 32
+print('Calculando...')
+print('%dºC = %dºF' %(celsius, fahrenheit))
